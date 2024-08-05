388d7e549308e87ed73c0be8432afbe2d4e9c1ff
Can Ivit <canivit@users.noreply.github.com>
2024-08-05 11:39:28 -0400

    add cli arguments to sagemaker script

--- face/run_sagemaker.py
+++ face/run_sagemaker.py
@@ -1,9 +1,32 @@
+import argparse
+
 import sagemaker
 from sagemaker.pytorch import PyTorch
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int)
+parser.add_argument('--batch-size', type=int)
+parser.add_argument('--learning-rate', type=float, default=0.001)
+parser.add_argument('--num-workers', type=int)
+parser.add_argument('--save-interval', type=int, default=100,
+                    help='Number of batches between checkpoints')
+parser.add_argument('--src-checkpoint', type=str, required=False)
+parser.add_argument('--dst-checkpoint', type=str, required=True)
+args = parser.parse_args()
+
+hyperparameters = {
+    "epochs": args.epochs,
+    'batch-size': args.batch_size,
+    'learning-rate': args.learning_rate,
+    'num-workers': args.num_workers,
+    'data-file': 'fer2013.csv',
+    'dst-checkpoint': args.dst_checkpoint
+}
+
+if args.src_checkpoint is not None:
+    hyperparameters['src-checkpoint'] = args.src_checkpoint
+
 session = sagemaker.Session()
-training_jobs = session.list_hubs()
-print(training_jobs)
 
 s3_input_data = 's3://etunes-bucket/data/fer2013.csv'
 s3_checkpoint_dir = 's3://etunes-bucket/checkpoints/'
@@ -20,15 +43,7 @@ pytorch_estimator = PyTorch(
     instance_count=1,
     # instance_type='ml.c5.4xlarge',
     instance_type='ml.p3.8xlarge',
-    hyperparameters={
-        'epochs': 400,
-        'batch-size': 64,
-        'learning-rate': 0.001,
-        'num-workers': 16,
-        'data-file': 'fer2013.csv',
-        'src-checkpoint': 'checkpoint_3.pt',
-        'dst-checkpoint': 'checkpoint_4.pt',
-    },
+    hyperparameters=hyperparameters,
     checkpoint_s3_uri=s3_checkpoint_dir,
 )
 
